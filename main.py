186c0a46e1848a2f68753b5402ed78a6b568944d
a774500050 <1627405006@stu.suda.edu.cn>
2020-04-22 02:21:32 +0800

    optimize value processing code prettify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import re
 
 def getHTMLText(url, code="utf-8", timeout=30):
     try:
@@ -13,13 +12,19 @@ def getHTMLText(url, code="utf-8", timeout=30):
         print('Error while getting html text: ', e)
         return ""
 
+
 def processTDLeft(tdObject):
-    text = tdObject.text.replace(": ","")
-    refDict={
-        "名称":"name",
-        "定价":"price",
-        "发售日":"data",
-        "比例":"scale",
+    '''
+    turn TDLeft's value into infoDict's Key
+    :param tdObject:
+    :return:formatted text
+    '''
+    text = tdObject.text.replace(": ", "")
+    refDict = {
+        "名称": "name",
+        "定价": "price",
+        "发售日": "data",
+        "比例": "scale",
         "原型": "prototyper",
         "原画": "illust",
         "角色": "chara",
@@ -34,25 +39,42 @@ def processTDRight(tdObject):
     text = tdObject.text.strip()
     return text
 
+def processKeyValue(key, value):
+    '''
+    process certain key's value
+    :param key:
+    :param value:
+    :return:formatted key and value
+    '''
+    if key == "size":
+        value = value[2:]
+        return key, value
+    if key == "price":
+        value = value.replace(",", "")[:value.find("日元") + 1]
+        return key, value
+    return key, value
+
 def getPVCInfo(url):
     infoDict = {
-        "name":"",
-        "price":"",
-        "data":"",
-        "scale":"",
+        "name": "",
+        "price": "",
+        "data": "",
+        "scale": "",
         "prototyper": "",
         "illust": "",
         "chara": "",
         "size": ""
     }
     html = getHTMLText(url)
-    mainTable = html.select("body > div.container.container-main > div.row > div.col-md-17 > div:nth-child(1) > div > div > div.col-xs-24.col-sm-15.col-md-15.col-lg-16 > table")
-#   select mainTable
-    trList = mainTable[0].find_all("tr") #form trList
+    mainTable = html.select(
+        "body > div.container.container-main > div.row > div.col-md-17 > div:nth-child(1) > div > div > "
+        "div.col-xs-24.col-sm-15.col-md-15.col-lg-16 > table")
+    #   select mainTable
+    trList = mainTable[0].find_all("tr")  # form trList
     for tr in trList:
         left = tr.select("td.info-box-left")
-        if len(left) != 0 :
-            #print(left)
+        if len(left) != 0:
+            # print(left)
             key = processTDLeft(left[0])
         else:
             continue
@@ -61,8 +83,9 @@ def getPVCInfo(url):
             value = processTDRight(right[0])
         else:
             continue
-        if key != None and value != None:
-#            print(key,value)
+        if key is not None and value is not None:
+            # print(key,value)
+            key, value = processKeyValue(key, value)
             infoDict[key] = value
     out = formResult(infoDict)
     return out
@@ -73,7 +96,7 @@ def formResult(infoDict):
 == 模型信息 ==
 {{{{模型周边信息|
 | 类别 = PVC
-| 系列 = 
+| 系列 = 无
 | 译名 = 
 | 原型师 = {infoDict.get('prototyper')}
 | 原画师 = {infoDict.get('illust')}
@@ -91,7 +114,8 @@ def formResult(infoDict):
 
     return result
 
-#url = "https://www.hpoi.net/hobby/38120"
+# url for test
+# url = "https://www.hpoi.net/hobby/38120"
 url = input("Please input HPOI Wiki URL:")
 
 print(getPVCInfo(url))
